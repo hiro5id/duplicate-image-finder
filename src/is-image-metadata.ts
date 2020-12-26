af98ac0237e52085e0c2102d05092f1cd1d36111
Roberto Sebestyen <roberto@sebestyen.ca>
2020-12-26 04:43:44 -0500

    fix: validator tests

--- src/is-image-metadata.ts
+++ src/is-image-metadata.ts
@@ -3,14 +3,14 @@ import Ajv from 'ajv';
 import imageMetadataSchema from './schemas/image-metadata.json';
 
 export function isImageMetadata(data: unknown): data is ImageMetadata {
-  // todo: need to use a validator library here using ajv
   const ajv = new Ajv({ allErrors: true });
+
   const valid = ajv.validate(imageMetadataSchema, data);
   if (valid) {
     return true;
   } else {
     const errorText = ajv.errorsText() && ajv.errorsText().toLowerCase() !== 'no errors' ? ajv.errorsText() : '';
-    console.log(errorText);
+    console.error(errorText);
     return false;
   }
 }
--- test/test-validator.ts
+++ test/test-validator.ts
@@ -1,9 +1,36 @@
-import { isImageMetadata } from '../src';
+import { ImageMetadata, isImageMetadata } from '../src';
 import { expect } from 'chai';
 
 describe('test validator', function () {
   it('is valid', function () {
-    const result = isImageMetadata({ blah: true });
+    const testData: ImageMetadata = {
+      pathInSearchDir: '',
+      fileMimeType: 'application/ogg',
+      fileName: '',
+      fullPath: '',
+      hash: {
+        binaryHash: '',
+        type: 'dhash',
+        version: 'v1',
+      },
+    };
+    const result = isImageMetadata(testData);
+    expect(result).eql(true);
+  });
+
+  it('is not valid', function () {
+    const testData: any = {
+      pathInSearchDir: '',
+      fileMimeType: 1,
+      fileName: '',
+      fullPath: '',
+      hash: {
+        binaryHash: '',
+        type: 'dhash',
+        version: 'v1',
+      },
+    };
+    const result = isImageMetadata(testData);
     expect(result).eql(false);
   });
 });
