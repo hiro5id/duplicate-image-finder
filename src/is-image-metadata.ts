113cc6a6f672ee6a5969ea444fb472f3d24138f5
Roberto Sebestyen <roberto@sebestyen.ca>
2020-12-26 06:28:14 -0500

    feat: improve schema generation to generate schema for all interfaces

--- src/is-image-metadata.ts
+++ src/is-image-metadata.ts
@@ -1,11 +1,13 @@
 import { ImageMetadata } from './image-metadata.interface';
 import Ajv from 'ajv';
-import imageMetadataSchema from './schemas/image-metadata.json';
+import imageMetadataSchema from './schemas/interface-schemas.json';
 
 export function isImageMetadata(data: unknown): data is ImageMetadata {
   const ajv = new Ajv({ allErrors: true });
 
-  const valid = ajv.validate(imageMetadataSchema, data);
+  const validate = ajv.addSchema(imageMetadataSchema).compile<ImageMetadata>(imageMetadataSchema.definitions.ImageMetadata);
+
+  const valid = validate(data);
   if (valid) {
     return true;
   } else {
