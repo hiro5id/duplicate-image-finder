f6bbfb38df6b9d54180b4001d8d8d544fde80b17
Roberto Sebestyen <roberto@sebestyen.ca>
2021-01-05 03:41:10 -0500

    docs: add comments

--- src/is-image-metadata.ts
+++ src/is-image-metadata.ts
@@ -6,17 +6,22 @@ import { getPropName } from './get-prop-name';
 import { ValidationError } from './validation-error.interface';
 
 export function isImageMetadata(data: unknown, errors?: ValidationError[]): data is ImageMetadata {
-  let ajv = new Ajv({ allErrors: true });
-  // Ajv option allErrors is required
-  require('ajv-errors')(ajv);
+  let ajv = new Ajv({ allErrors: true }); // Ajv option allErrors is required
+  require('ajv-errors')(ajv); // better validation errors
 
+  // load validation schema
   ajv = ajv.addSchema(imageMetadataSchema);
   const validate = ajv.compile(imageMetadataSchema.definitions.ImageMetadata);
 
+  // validate
   const valid = validate(data);
+
+  // handle validation
   if (valid) {
+    // everything OK
     return true;
   } else {
+    // show validation errors
     const errMsg = `validation failed for ${getPropName(imageMetadataSchema.definitions, imageMetadataSchema.definitions.ImageMetadata)}`;
     if (isArray(validate.errors)) {
       const validationErrors = validate.errors.map(m => {
